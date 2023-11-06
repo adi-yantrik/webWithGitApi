3194230531e02ef8567630b220ebfa170a949fd3
adi-yantrik <121053124+adi-yantrik@users.noreply.github.com>
2023-11-06 21:03:00 +0545

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ function bindData(articles) {
 function fillDataInCard(cardClone, article) {
     const newsImg = cardClone.querySelector("#news-img");
     const newsTitle = cardClone.querySelector("#news-title");
-    const newsSource = cardClone.querySelector("#news-source");
     const newsDesc = cardClone.querySelector("#news-desc");
 
     newsImg.src = article.urlToImage;
